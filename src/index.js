90241d9582c1d52f95a8361ee92a79d8888ec74c
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-09-23 15:25:43 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,22 +1,28 @@
-// In src/index.js
 const express = require("express");
 const { v4: uuidv4 } = require('uuid');
 
-const uuid = uuidv4();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// *** REMOVE ***
+const logs = []; // Array to store visitor logs
+
 app.get("/", (req, res) => {
-  let data={};
-    data['latitude'] = req.headers['x-vercel-ip-latitude'];
-  data['longitude'] = req.headers['x-vercel-ip-longitude'];
-  data['location'] = req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country'];
-  data['ip_address'] = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  data['uuid']=uuid;
-  res.send(data);
+  const log = {
+    latitude: req.headers['x-vercel-ip-latitude'],
+    longitude: req.headers['x-vercel-ip-longitude'],
+    location: req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country'],
+    ip_address: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+    uuid: uuidv4()
+  };
+
+  logs.push(log); // Add the log to the array
+
+  res.send(log);
 });
 
+app.get("/logs", (req, res) => {
+  res.send(logs);
+});
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
