6f3d152ff3a43b1d80e05d385ca78d3b5a377a93
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-09-23 15:22:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // In src/index.js
 const express = require("express");
+const { v4: uuidv4 } = require('uuid');
 
-
+const uuid = uuidv4();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -12,6 +13,7 @@ app.get("/", (req, res) => {
   data['longitude'] = req.headers['x-vercel-ip-longitude'];
   data['location'] = req.headers['x-vercel-ip-city'] + ',' + req.headers['x-vercel-ip-country-region'] + ',' + req.headers['x-vercel-ip-country'];
   data['ip_address'] = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  data['uuid']=uuid;
   res.send(data);
 });
 
